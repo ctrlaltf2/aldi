e3d00fb9a1dfe95b899308eed7b6984d654eb740
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2025-04-14 21:15:30 -0400

    chore: ruff format

--- scrape.py
+++ scrape.py
@@ -19,48 +19,56 @@ import random
 import time
 
 parser = argparse.ArgumentParser(
-    prog='aldi-scraper',
-    description='Pull down current prices at a specific ALDI store'
+    prog="aldi-scraper", description="Pull down current prices at a specific ALDI store"
 )
 
 # store region, found by choosing a store for pickup near you, and looking at the store code in cookies.
 # An example total store identifier on the West coast might be 479-030, with 479 being the region code
-parser.add_argument('-r', '--region', type=int, required=True, help='Region number (e.g. 479)')
+parser.add_argument(
+    "-r", "--region", type=int, required=True, help="Region number (e.g. 479)"
+)
 # See above. If your specific store doesn't show up in the search,
 # check your physical receipt for the store number.
-parser.add_argument('-s', '--store', type=int, required=True, help='Store number (e.g. 40)')
-parser.add_argument('-d', '--db', type=str, required=True, help='Path to output prices database')
+parser.add_argument(
+    "-s", "--store", type=int, required=True, help="Store number (e.g. 40)"
+)
+parser.add_argument(
+    "-d", "--db", type=str, required=True, help="Path to output prices database"
+)
 args = parser.parse_args()
 
 output_db_path = Path(args.db)
 
 # --- Store config
-region_number = f'{args.region:03}'
-store_number  = f'{args.store:03}'
+region_number = f"{args.region:03}"
+store_number = f"{args.store:03}"
 # This is the usual max page limit
 page_limit = 60
 # ---
 
 # --- Scraper config
-min_sleep = 2433 # ms
-max_sleep = 6151 # ms
+min_sleep = 2433  # ms
+max_sleep = 6151  # ms
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0',
-    'Accept': 'application/json, text/plain, */*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
-    'Origin': 'https://new.aldi.us',
-    'Sec-GPC': '1',
-    'Connection': 'keep-alive',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-site',
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0",
+    "Accept": "application/json, text/plain, */*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Origin": "https://new.aldi.us",
+    "Sec-GPC": "1",
+    "Connection": "keep-alive",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
 }
-url = Template('https://api.aldi.us/v3/product-search?currency=USD&q=&limit=$limit&offset=$offset&sort=name_asc&servicePoint=$region-$store')
+url = Template(
+    "https://api.aldi.us/v3/product-search?currency=USD&q=&limit=$limit&offset=$offset&sort=name_asc&servicePoint=$region-$store"
+)
 # ---
 
 end_index = None
 current_index = 0
+
 
 # track ephemeral failures and do exponential backoff random retry
 def gen_time(failures: int, min_sleep: float, max_sleep: float) -> float:
@@ -70,33 +78,34 @@ def gen_time(failures: int, min_sleep: float, max_sleep: float) -> float:
 
     return sleep_time
 
+
 with duckdb.connect(output_db_path) as conn:
     create_table_products = (
         "CREATE TABLE IF NOT EXISTS products ("
-            "timestamp TIMESTAMPTZ, "
-            "storeCode VARCHAR, "
-            "sku VARCHAR, "
-            "name VARCHAR, "
-            "brandName VARCHAR, "
-            "urlSlugText VARCHAR, "
-            "discontinued BOOLEAN, "
-            "discontinuedNote VARCHAR, "
-            "notForSale BOOLEAN, "
-            "notForSaleReason VARCHAR, "
-            "quantityMin FLOAT, "
-            "quantityMax FLOAT, "
-            "quantityInterval FLOAT, "
-            "quantityDefault FLOAT, "
-            "quantityUnit VARCHAR, "
-            "weightType VARCHAR, "
-            "sellingSize VARCHAR, "
-            "priceAmount INTEGER, "
-            "priceAmountRelevant INTEGER, "
-            "priceComparison INTEGER, "
-            "priceCurrencyCode VARCHAR, "
-            "pricePerUnit FLOAT, "
-            "pricePerUnitDisplay VARCHAR, "
-            "assets STRUCT(url VARCHAR, maxWidth BIGINT, maxHeight BIGINT, mimeType VARCHAR, assetType VARCHAR)[]"
+        "timestamp TIMESTAMPTZ, "
+        "storeCode VARCHAR, "
+        "sku VARCHAR, "
+        "name VARCHAR, "
+        "brandName VARCHAR, "
+        "urlSlugText VARCHAR, "
+        "discontinued BOOLEAN, "
+        "discontinuedNote VARCHAR, "
+        "notForSale BOOLEAN, "
+        "notForSaleReason VARCHAR, "
+        "quantityMin FLOAT, "
+        "quantityMax FLOAT, "
+        "quantityInterval FLOAT, "
+        "quantityDefault FLOAT, "
+        "quantityUnit VARCHAR, "
+        "weightType VARCHAR, "
+        "sellingSize VARCHAR, "
+        "priceAmount INTEGER, "
+        "priceAmountRelevant INTEGER, "
+        "priceComparison INTEGER, "
+        "priceCurrencyCode VARCHAR, "
+        "pricePerUnit FLOAT, "
+        "pricePerUnitDisplay VARCHAR, "
+        "assets STRUCT(url VARCHAR, maxWidth BIGINT, maxHeight BIGINT, mimeType VARCHAR, assetType VARCHAR)[]"
         ");"
     )
 
@@ -105,22 +114,22 @@ with duckdb.connect(output_db_path) as conn:
     failures = 0
     while (not end_index) or (current_index <= end_index):
         sleep_time = gen_time(failures, min_sleep, max_sleep)
-        print(f'Sleeping for {sleep_time}s...')
+        print(f"Sleeping for {sleep_time}s...")
         time.sleep(sleep_time)
         this_url = url.substitute(
-                {
-                    'limit': page_limit,
-                    'offset': current_index,
-                    'region': region_number,
-                    'store': store_number,
-                }
+            {
+                "limit": page_limit,
+                "offset": current_index,
+                "region": region_number,
+                "store": store_number,
+            }
         )
-        print(f'GET {this_url}')
+        print(f"GET {this_url}")
         response = httpx.get(this_url, headers=headers)
 
         # Exp retry last request if error
         if response.status_code != 200:
-            print('Failed, retrying.')
+            print("Failed, retrying.")
             failures += 1
             continue
         else:
@@ -129,13 +138,13 @@ with duckdb.connect(output_db_path) as conn:
         js = response.json()
 
         if end_index is None:
-            end_index = int(js['meta']['pagination']['totalCount'])
+            end_index = int(js["meta"]["pagination"]["totalCount"])
 
         # Hacky way to get the json readable from duckdb read_json
-        with fsspec.filesystem('memory').open(f'{str(current_index)}.json', 'w') as f:
+        with fsspec.filesystem("memory").open(f"{str(current_index)}.json", "w") as f:
             json.dump(js, f)
 
-        conn.register_filesystem(fsspec.filesystem('memory'))
+        conn.register_filesystem(fsspec.filesystem("memory"))
 
         insert = f"""
             INSERT INTO products WITH actual_table AS (
